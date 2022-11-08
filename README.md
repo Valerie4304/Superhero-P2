6bf27fcaf74fbc08b6f3bdb64e8e6f6067acc873
Valerie4304 <vcruz21@uic.edu>
2022-11-07 19:00:13 -0600

    This is my project 2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SuperheroGame-P2
 This is my project 2
+This is a text based game where the player picks one of two choices. One option is correct and it 
+will allow the player to continue playing. The other option is incorrect and requires the player 
+to revive if they wish to continue playing. 
+The player is a superhero and the objective is to defeat the villain.
--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ h1.style.color = 'black';
 h1.style.backgroundColor = (245, 222, 92);
 }
 
-let counter = 5;
+let counter = 10;
 
 function timeout() {
     setTimeout(function(){
         document.getElementById('countDown').textContent = counter;
         counter--;
         if (counter < 0){
-            alert('Times run out. You lost.');
-            window.location.replace('nextPage.html');
+            alert('Out of time');
+            window.location.replace('page6.html');
         }
      timeout();   
     }, 1000);
